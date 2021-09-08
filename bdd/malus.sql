462bc16035f56d062ef6cdab3d71ba3a31bc8391
Erwan8989 <77726753+Erwan8989@users.noreply.github.com>
2021-09-08 12:16:20 +0200

    Update malus.sql

--- bdd/malus.sql
+++ bdd/malus.sql
@@ -3,8 +3,8 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : mer. 08 sep. 2021 à 09:52
--- Version du serveur :  5.7.31
+-- Généré le : mer. 08 sep. 2021 à 10:15
+-- Version du serveur :  8.0.21
 -- Version de PHP : 7.3.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -29,9 +29,9 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `malus`;
 CREATE TABLE IF NOT EXISTS `malus` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `titre` text COLLATE utf8_unicode_ci NOT NULL,
-  `instruction` text COLLATE utf8_unicode_ci NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `titre` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `instruction` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
@@ -43,7 +43,6 @@ INSERT INTO `malus` (`id`, `titre`, `instruction`) VALUES
 (1, 'Mauvaise réponse !', 'Joue avec une main dans le dos jusqu\'au prochain tour.'),
 (2, 'Mauvaise réponse ! ', 'Et si tu jouais jusqu\'au prochain but en équilibre sur un pied ?'),
 (3, 'Mauvaise réponse !', 'Les deux joueurs ferment leurs yeux pendant 15 secondes.'),
-(4, '', ''),
 (5, 'Mauvaise réponse ! ', 'Un des deux joueurs fait 2 tours du babyfoot au début du prochain tour !'),
 (6, 'Mauvaise réponse ! ', 'N\'utilisez que les défenseur et les milieux de terrain pour jouer.'),
 (7, 'Mauvaise réponse ! ', 'Mets toi dos au babyfoot pour jouer lors de la prochaine partie.'),
@@ -51,7 +50,6 @@ INSERT INTO `malus` (`id`, `titre`, `instruction`) VALUES
 (9, 'Mauvaise réponse !', 'Malheureusement tu perds 1 point.'),
 (10, 'Mauvaise réponse ! ', 'Les joueurs se mettent en position de squat pendant 20 secondes.'),
 (11, 'Mauvaise réponse !', 'Les joueurs se mettent en position de la chaise jusqu\'au au prochain but.'),
-(12, 'Mauvaise réponse ! ', 'Rajoutez le nombre de balles en plus dont vous disposez ( max 3 ), une balle rentrée = 1 point.\r\n'),
 (13, 'Mauvaise réponse ! ', 'Un des deux joueurs ne peux plus jouer pendant 20 secondes.'),
 (14, 'Mauvaise réponse ! ', 'Le joueur en défense échange sa main droite avec la main gauche de son co-équipier.');
 COMMIT;
